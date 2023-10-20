0f0fcc61e68604626005ad38e56e32c06dfecb57
IrynaMe <irinam7a@gmail.com>
2023-10-20 16:29:19 +0200

    4

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -43,11 +43,8 @@ public class FileReader {
 
             return new Profile(name, age, email, phone);
         }
-        catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
         catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Error during reading data from file", e);
         }
     }
 }
