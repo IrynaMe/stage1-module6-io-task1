608e60f4cf19f2dd1dcb158098bc6690d8af0a63
IrynaMe <irinam7a@gmail.com>
2023-10-20 10:34:56 +0200

    1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,52 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
+// https://github.com/IrynaMe/stage1-module6-io-task1/blob/21a894d16d9cc18db489ffabe46e5096bb9caa23/src/main/resources/Profile.txt
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+
+        try (FileInputStream fileInputStream = new FileInputStream(file);
+             InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+             BufferedReader reader = new BufferedReader(inputStreamReader)) {
+            String line;
+            String name = null;
+            Integer age = null;
+            String email = null;
+            Long phone = null;
+
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(":");
+                if (parts.length == 2) {
+                    String key = parts[0].trim();
+                    String value = parts[1].trim();
+                    switch (key) {
+                        case "Name":
+                            name = value;
+                            break;
+                        case "Age":
+                            age = Integer.parseInt(value);
+                            break;
+                        case "Email":
+                            email = value;
+                            break;
+                        case "Phone":
+                            phone = Long.parseLong(value);
+                            break;
+                        default:
+                            System.out.println("Unknown key");
+                            break;
+                    }
+                }else {
+                    System.out.println("Unknown line format");
+                }
+            }
+
+            return new Profile(name, age, email, phone);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
